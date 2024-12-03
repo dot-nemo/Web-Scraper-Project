84ceb76af6b5c086a9d41642a0740b9d97bea59f
nemo / stan <stanpai78@gmail.com>
2024-12-04 02:16:30 +0800

    Update: Oh the misery

--- _email_extractor.py
+++ _email_extractor.py
@@ -1,63 +1,73 @@
 from dlsu_website.spiders.dlsu_website import website_queue
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from dlsu_website.spiders.cf_email import EmailSpider
 from scrapy.signalmanager import dispatcher
 from scrapy import signals
-from toCsv import ToCSV
 import pika, json
+from twisted.internet import reactor, threads
 
-import threading, time
 
-import csv
+class WebsiteConsumer:
+    def __init__(self, id, toCsv=None):
+        self.result_dict = {}
+        self.thread_id = id
+        self.toCsv = toCsv
+        self.count = 0
 
-class WebsiteConsumer():
-  def __init__(self, id, toCsv=None):
-    self.result_dict={}
-    self.thread_id = id
-    self.toCsv = toCsv
-    self.count = 0
+    def process_url(self, url):
+        runner = CrawlerRunner(get_project_settings())
+        results = []
 
-  def processURL(self, url):
-    process = CrawlerProcess(get_project_settings())
-    results = []
-    def crawler_results(signal, sender, item, response, spider):
-        if self.thread_id == spider.id:
-          results.append(item)
+        def crawler_results(signal, sender, item, response, spider):
+            if self.thread_id == spider.id:
+                results.append(item)
 
-    dispatcher.connect(crawler_results, signal=signals.item_scraped)
+        dispatcher.connect(crawler_results, signal=signals.item_scraped)
 
-    process.crawl(EmailSpider, url=url, id=self.thread_id)
+        # Schedule the crawl
+        d = runner.crawl(EmailSpider, url=url, id=self.thread_id)
 
-    if len(results) > 0:
-      for item in results:
-        email = item["email"]
-        del item["email"]
-        self.result_dict[email] = item
-        # self.toCsv.addItem(email, self.result_dict[email]['firstname'], self.result_dict[email]['lastname'])
+        # Process results after crawl completes
+        d.addCallback(lambda _: self._process_results(results))
+        d.addErrback(lambda error: print(f"Error in spider: {error}"))
 
-    self.count += 1
+    def _process_results(self, results):
+        if results:
+            for item in results:
+                email = item["email"]
+                del item["email"]
+                self.result_dict[email] = item
+                # Optional: Process result for CSV
+                # self.toCsv.addItem(email, self.result_dict[email]['firstname'], self.result_dict[email]['lastname'])
+        self.count += 1
 
-  def getCount(self):
-    return self.count
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        body_dict = json.loads(body)
+        url = body_dict["url"]
 
-  def callback(self, ch, method, properties, body):
-    print(" [x] Received %r" % body)
-    body_dict = json.loads(body)
-    url = body_dict["url"]
-    self.processURL(url)
+        # Schedule processing on the reactor thread
+        reactor.callFromThread(self.process_url, url)
 
-credentials = pika.PlainCredentials('rabbituser', 'rabbit1234')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.2.202.75',5672,'/',credentials))
-channel = connection.channel()
+def start_pika_consumer():
+    """Starts the Pika consumer."""
+    credentials = pika.PlainCredentials("rabbituser", "rabbit1234")
+    connection = pika.BlockingConnection(pika.ConnectionParameters("10.2.202.75", 5672, "/", credentials))
+    channel = connection.channel()
 
-channel.queue_declare(queue='rqueue')
+    channel.queue_declare(queue="rqueue")
 
-webcon = WebsiteConsumer()
+    webcon = WebsiteConsumer(id=1)
+    channel.basic_consume(queue="rqueue", on_message_callback=webcon.callback, auto_ack=True)
 
-callback = webcon.callback
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
 
-channel.basic_consume('rqueue', callback, True)
 
-channel.start_consuming()
+# Run Pika consumer in a separate thread
+threads.deferToThread(start_pika_consumer)
+
+# Start the Twisted reactor
+reactor.run()
