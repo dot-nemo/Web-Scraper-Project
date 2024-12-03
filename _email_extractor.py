8c00a840f2643d8f7e767fc8feb97faf0f475ed6
nemo / stan <stanpai78@gmail.com>
2024-12-03 21:00:28 +0800

    Add: set id of email extractor via cl arguments

--- _email_extractor.py
+++ _email_extractor.py
@@ -1,6 +1,10 @@
 from website_consumer import WebsiteConsumer
+import sys
 
 if __name__ == "__main__":
-  t = WebsiteConsumer(0)
+  id = 0
+  if len(sys.argv) > 1:
+    id = int(sys.argv[1])
+  t = WebsiteConsumer(id)
   t.start()
   t.join()
