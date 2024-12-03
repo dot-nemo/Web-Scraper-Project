e4e54778dde4fa77803024ec28f9cfd3fde55647
nemo / stan <stanpai78@gmail.com>
2024-12-03 19:42:07 +0800

    Update: consumer to ack messages

--- website_consumer.py
+++ website_consumer.py
@@ -30,7 +30,7 @@ class WebsiteConsumer(threading.Thread):
 
       self.channel.queue_declare(queue='rqueue')
       print("Connected to RabbitMQ")
-      self.channel.basic_consume('rqueue', self.callback)
+      self.channel.basic_consume('rqueue', True, self.callback)
     except:
       print("Unable to connect to RabbitMQ")
 
